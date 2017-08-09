cbf0b4fe455ea6c0340e577c40631d551799a7ca
llright <wright.l@husky.neu.edu>
2017-08-09 16:31:54 -0400

    put lines in catagories and get average for each catagory

--- edge_test.py
+++ edge_test.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 import cozmo
+from cozmo.util import degrees, distance_mm, speed_mmps
 from PIL import ImageDraw, ImageOps
 import PIL.Image
 import PIL.ImageFont
@@ -8,6 +9,7 @@ import PIL.ImageTk
 import cv2
 import numpy as np
 import tkinter
+from array import array
 
 # Run Configuration
 os.environ['COZMO_PROTOCOL_LOG_LEVEL'] = 'DEBUG'
@@ -26,8 +28,12 @@ Edge Test
 -Experimenting with Cozmo's camera and OpenCV
 -Displays TKinter window with both pre and post-processed live camera feed
 -
-@author Team Cozplay
+@author Lindsay Wright
 '''
+horizontal_mean_over_time=[]
+right_mean_over_time=[]
+left_mean_over_time=[]
+
 
 
 class EdgeTest:
@@ -45,8 +51,9 @@ class EdgeTest:
         raw_image_no_crop = image.raw_image
         #undistort raw image
         cv2_undistort = cv2.cvtColor(np.array(raw_image_no_crop), cv2.COLOR_RGB2BGR) #convert to openCV format
-        cv2_image = self.undistort_image(cv2_undistort) #undistort
-        cv2_image=cv2_image[80:235, 5:315] #320 width 240 height
+        # cv2_image = self.undistort_image(cv2_undistort) #undistort
+        cv2_image = cv2_undistort[80:235, 5:315]  # 320 width 240 height
+        # cv2_image=cv2_image[80:235, 5:315] #320 width 240 height
 
         # Apply edge filter
         cv2_edges = self.auto_canny(cv2_image)
@@ -67,21 +74,7 @@ class EdgeTest:
         pil_edges = PIL.Image.fromarray(cv2.cvtColor(cv2_edges, cv2.COLOR_GRAY2RGB))
 
         # Display input and output feed
-        draw = ImageDraw.Draw(image.raw_image)
-        a, b, c = cv2_lines.shape
-        for i in range(a):
-            x1 = cv2_lines[i][0][0]+5
-            y1 = cv2_lines[i][0][1]+80
-            x2 = cv2_lines[i][0][2]+5
-            y2 = cv2_lines[i][0][3]+80
-            if (x1-x2)<10 and (x1-x2)>-10: #vertical
-                draw.line([(x1, y1), (x2, y2)], fill=(0, 128, 0), width=4) #green
-            elif (y1-y2)<10 and (y1-y2)>-10: #horizontal
-                draw.line([(x1, y1), (x2, y2)], fill=(128,0, 128), width=4) #purple
-            elif ((y1-y2)/(x1-x2)) > 0: # and ((y1-y2)/(x1-x2)) <-75:
-                draw.line([(x1, y1), (x2, y2)], fill=(135, 206, 250), width=4) #light blue
-            else:
-                draw.line([(x1, y1), (x2, y2)], fill=(255, 0, 0), width=4) #red
+        self.catagorize_lines(cv2_lines,image.raw_image)
         display_image_input = PIL.ImageTk.PhotoImage(image=image.raw_image)
         display_image_output = PIL.ImageTk.PhotoImage(image=pil_edges)
 
@@ -96,6 +89,10 @@ class EdgeTest:
         self._tk_label_output.imgtk = display_image_output
         self._tk_label_output.configure(image=display_image_output)
         self._tk_root.update()
+
+
+
+
 
     # Auto-paramter Canny edge detection adapted from:
     # http://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
@@ -122,13 +119,76 @@ class EdgeTest:
         undistorted_image = cv2.undistort(img, cameraMatrix, distCoef)
         return undistorted_image
 
+    #catagorizes the lines and computes the average of each type, updates the list of averages
+    def catagorize_lines(self, lines, draw_on):
+        draw = ImageDraw.Draw(draw_on)
+        a, b, c = lines.shape
+        vertical, horizontal, left, right = [],[],[],[]
+        #catagorize lines into types
+        for i in range(a):
+            x1 = lines[i][0][0]+5
+            y1 = lines[i][0][1]+80
+            x2 = lines[i][0][2]+5
+            y2 = lines[i][0][3]+80
+            if (x1-x2)<10 and (x1-x2)>-10: #vertical
+                vertical.append([x1,y1,x2,y2])
+
+            elif (y1-y2)<6 and (y1-y2)>-6: #horizontal
+                horizontal.append([x1,y1,x2,y2])
+
+            elif ((y1-y2)/(x1-x2)) > 0.3: #right
+                right.append([x1,y1,x2,y2])
+
+            elif ((y1-y2)/(x1-x2)) < -0.3: #left
+                left.append([x1,y1,x2,y2])
+
+        #compute mean of all line types if they exist, then draw the mean of the last 15 means
+        if vertical:
+            vertical_mean=np.mean(vertical, axis=0,dtype=np.int)
+            draw.line([(vertical_mean[0], vertical_mean[1]), (vertical_mean[2], vertical_mean[3])], fill=(0, 128, 0), width=4)  # Vertical green
+        if horizontal: #if there was horizontal lines found
+            horizontal_mean = np.mean(horizontal, axis=0,dtype=np.int) #calculate the mean of this frame
+            horizontal_mean_over_time.append(horizontal_mean) #add it to the list that saves previous frames
+            if len(horizontal_mean_over_time) > 10: #if that list gets over 15, then dump the oldest number
+                horizontal_mean_over_time.pop(0)
+            horizontal_mean = np.mean(horizontal_mean_over_time, axis=0, dtype=np.int)
+            draw.line([(horizontal_mean[0], horizontal_mean[1]), (horizontal_mean[2], horizontal_mean[3])],fill=(128, 0, 128), width=4)  # Horizontal purple
+        if not horizontal:
+            horizontal_mean_over_time[:]=[]
+        if right:
+            right_mean = np.mean(right, axis=0,dtype=np.int)
+            right_mean_over_time.append(right_mean)  # add it to the list that saves previous frames
+            if len(right_mean_over_time) > 10:  # if that list gets over 5, then dump the oldest number
+                 right_mean_over_time.pop(0)
+            right_mean = np.mean(right_mean_over_time, axis=0, dtype=np.int)
+            draw.line([(right_mean[0], right_mean[1]), (right_mean[2], right_mean[3])], fill=(135, 206, 250), width=4)  # Right light blue
+        if not right:
+            right_mean_over_time[:]=[]
+        if left:
+            left_mean =np.mean(left, axis=0,dtype=np.int)
+            left_mean_over_time.append(left_mean)  # add it to the list that saves previous frames
+            if len(left_mean_over_time) > 10:  # if that list gets over 15, then dump the oldest number
+                 left_mean_over_time.pop(0)
+            left_mean = np.mean(left_mean_over_time, axis=0, dtype=np.int)
+            draw.line([(left_mean[0], left_mean[1]), (left_mean[2], left_mean[3])], fill=(255, 0, 0), width=4)  # Left red
+        if not left:
+            left_mean_over_time[:]=[]
+
+    def straighten_out(self):
+        if len(left_mean_over_time) == 0 and len(right_mean_over_time) > 0:
+            cozmo.robot.Robot.turn_in_place(angle=degrees(5),in_parallel=True,num_retries=0).wait_for_completed() #turn left
+        if len(right_mean_over_time) == 0 and len(left_mean_over_time) > 0:
+            cozmo.robot.Robot.turn_in_place(angle=degrees(-5), in_parallel=True, num_retries=0).wait_for_completed() #turn right
+
+
+
     async def set_up_cozmo(self, coz_conn):
         asyncio.set_event_loop(coz_conn._loop)
         self._robot = await coz_conn.wait_for_robot()
         self._robot.camera.image_stream_enabled = True
         self._robot.add_event_handler(cozmo.world.EvtNewCameraImage, self.on_new_camera_image)
         self._robot.set_head_angle(cozmo.util.Angle(degrees=0))
-       # self._robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+
 
     async def run(self, coz_conn):
         # Set up Cozmo
@@ -140,6 +200,7 @@ class EdgeTest:
         self._tk_label_output = tkinter.Label(self._tk_root)
         self._tk_label_input.pack()
         self._tk_label_output.pack()
+        #self.straighten_out()
 
         while True:
             await asyncio.sleep(0)
@@ -147,4 +208,7 @@ class EdgeTest:
 
 if __name__ == '__main__':
     EdgeTest()
+
+
+
 
